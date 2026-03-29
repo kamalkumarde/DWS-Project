4a90901846a1cea4c199ae4f3d6628010b94db04
kamalkumarde <madhavankamalkumar@gmail.com>
2026-03-29 20:10:11 +0200

    infra: ensure module source code is tracked

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -2,8 +2,8 @@ variable "project_id" {}
 variable "bucket_name" {}
 
 resource "google_storage_bucket" "bucket" {
-  project  = var.project_id
-  name     = var.bucket_name
-  location = "US"
+  project                     = var.project_id
+  name                        = var.bucket_name
+  location                    = "US"
   uniform_bucket_level_access = true
 }
