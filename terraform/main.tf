985a3d5077ab77f03da14a802ad3da9ef0607419
kamalkumarde <madhavankamalkumar@gmail.com>
2026-03-30 04:07:28 +0200

    tf version upgrade

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = "us-central1"
+}
+
 module "warehouse" {
   source     = "./modules/bigquery"
   project_id = var.project_id
